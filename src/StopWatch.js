2185156cad1a87ef0aca6505cfe5a2b84802b61f
onehae627 <onehae627@gmail.com>
2022-12-13 20:57:32 +0900

    일시정지

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,9 +3,24 @@ import React, { useState } from "react";
 function StopWatch() {
   const [num, setNum] = useState(0);
   
-  setTimeout(() => setNum(num + 1), 1000);
+  const timeoutID = setTimeout(() => setNum(num + 1), 1000);
   
-  return <>숫자 : {num}</>;
+  const pause = () => clearTimeout(timeoutID);
+
+  const resume = () => {
+    setTimeout(() => setNum(num + 1), 1000);
+  };
+
+  
+  return (
+    <>
+      숫자 : {num}
+      <hr />
+      <button onclick={pause}>일시정지</button>
+      
+    </>
+  );
+  
 }
 
 export default StopWatch;
