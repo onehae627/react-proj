3269f1e9bb9f77785e0d6781581c1e8ac8d88c95
onehae627 <onehae627@gmail.com>
2022-12-15 20:15:58 +0900

    닫기버튼 누르면 팝업 제거

---
+++ src/Popup.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function Popup() {
+  const border = "10px solid red";
+  const [popupVisible, setPopupVisible] = useState(true);
+  
+  return (
+    <>
+      <button className="btn btn-outline"
+      onClick={() => setPopupVisible(false)}>
+        팝업닫기
+        </button>
+      <hr />
+      {popupVisible && <div style={{ width: 100, height: 100, border }}></div>}
+    </>
+  );
+  
+}
+
+
+  export default Popup;
+
