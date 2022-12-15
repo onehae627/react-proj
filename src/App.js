1157b3d0cdc3697262174f49534049c3546e3fe8
onehae627 <onehae627@gmail.com>
2022-12-15 21:26:55 +0900

    상품리스트 스타일 설정

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
       {/* <Popup /> */}
       <div style={{display: "flex", gap: "10px"}}>
      <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
-      <img src="https://picsum.photos/id/237/400/400"/>
-      <div style={{textAlign: "center"}}>MAC BOOK</div>
-      <div style={{textAlign: "center"}}>4,340,000</div>
-     </div>
-     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
       <img src="https://picsum.photos/id/1/400/400"/>
-      <div style={{textAlign: "center"}}>MAC BOOK PRO</div>
+      <div style={{textAlign: "center" ,fontWeight:"bold", color: "#454545"}}>MAC BOOK</div>
       <div style={{textAlign: "center"}}>4,340,000</div>
      </div>
      <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
       <img src="https://picsum.photos/id/2/400/400"/>
-      <div style={{textAlign: "center"}}>MAC BOOK PRO MAX</div>
+      <div style={{textAlign: "center",fontWeight:"bold", color: "#454545"}}>MAC BOOK PRO</div>
+      <div style={{textAlign: "center"}}>4,340,000</div>
+     </div>
+     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
+      <img src="https://picsum.photos/id/3/400/400"/>
+      <div style={{textAlign: "center",fontWeight:"bold", color: "#454545"}}>MAC BOOK PRO MAX</div>
       <div style={{textAlign: "center"}}>4,340,000</div>
      </div>
      </div>
