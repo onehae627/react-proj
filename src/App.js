60f00529056b408a49271fa4dbe92bdc1b9eddd8
onehae627 <onehae627@gmail.com>
2022-12-15 21:18:52 +0900

    상품리스트 구현2

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ function App() {
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
       {/* <Popup /> */}
+      <div style={{display: "flex", gap: "10px"}}>
      <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
       <img src="https://picsum.photos/id/237/400/400"/>
-      <div style={{textAlign: "center"}}>MAC OS</div>
+      <div style={{textAlign: "center"}}>MAC BOOK</div>
       <div style={{textAlign: "center"}}>4,340,000</div>
+     </div>
+     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
+      <img src="https://picsum.photos/id/1/400/400"/>
+      <div style={{textAlign: "center"}}>MAC BOOK PRO</div>
+      <div style={{textAlign: "center"}}>4,340,000</div>
+     </div>
+     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
+      <img src="https://picsum.photos/id/2/400/400"/>
+      <div style={{textAlign: "center"}}>MAC BOOK PRO MAX</div>
+      <div style={{textAlign: "center"}}>4,340,000</div>
+     </div>
      </div>
 
     </>
