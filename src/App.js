69e2755ea2cddb0bf3969a34cb4b64a02e178084
onehae627 <onehae627@gmail.com>
2022-12-15 21:16:17 +0900

    상품리스트 구현1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
     <>
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
-      <Popup />
-     
+      {/* <Popup /> */}
+     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
+      <img src="https://picsum.photos/id/237/400/400"/>
+      <div style={{textAlign: "center"}}>MAC OS</div>
+      <div style={{textAlign: "center"}}>4,340,000</div>
+     </div>
 
     </>
   );
---
+++ src/StopWatch copy.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function StopWatch() {
+  const [num, setNum] = useState(0);
+  
+  let timeoutId = setTimeout(() => setNum(num + 1), 1000);
+  
+  const pause = () => clearTimeout(timeoutId);
+
+  const resume = () => {
+    timeoutId = setTimeout(() => setNum(num + 1), 1000);
+  };
+
+  
+  return (
+    <>
+      숫자 : {num}
+      <hr />
+      <button onclick={pause}>일시정지</button>
+      <hr />
+      <button onClick={resume}>재개</button>
+    </>
+  );
+  
+}
+
+export default StopWatch;
+
