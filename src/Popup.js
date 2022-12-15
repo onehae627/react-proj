1601f51185a39732265b9389432cc629ea806451
onehae627 <onehae627@gmail.com>
2022-12-15 20:19:46 +0900

    팝업열기 만들기 하지만 실패

--- src/Popup.js
+++ src/Popup.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Popup() {
   const border = "10px solid red";
   const [popupVisible, setPopupVisible] = useState(true);
+  const [popupOpen, setPopupOpen] = useState(false);
   
   return (
     <>
@@ -10,8 +11,15 @@ function Popup() {
       onClick={() => setPopupVisible(false)}>
         팝업닫기
         </button>
+        <button className="btn btn-outline"
+      onClick={() => setPopupVisible(true)}>
+        팝업닫기
+        </button>
       <hr />
       {popupVisible && <div style={{ width: 100, height: 100, border }}></div>}
+      {popupOpen || <div style={{ width: 100, height: 100, border }}></div>}
+      
+
     </>
   );
   
