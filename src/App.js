9269932f128c0316c4cbd196a052cd5b0d39899c
onehae627 <onehae627@gmail.com>
2022-12-16 17:14:11 +0900

    상품리스트품목을 별도의 컴포넌트로 제작

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import StopWatch from "./StopWatch";
 import NumberCounter from "./NumberCounter";
 import Popup from "./Popup";
+import ProductListItem from "./ProductListItem";
 function App() {
   return (
     <>
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
       {/* <Popup /> */}
-      <div style={{display: "flex", gap: "10px"}}>
-     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
-      <img src="https://picsum.photos/id/1/400/400"/>
-      <div style={{textAlign: "center" ,fontWeight:"bold", color: "#454545"}}>MAC BOOK</div>
-      <div style={{textAlign: "center"}}>4,340,000</div>
-     </div>
-     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
-      <img src="https://picsum.photos/id/2/400/400"/>
-      <div style={{textAlign: "center",fontWeight:"bold", color: "#454545"}}>MAC BOOK PRO</div>
-      <div style={{textAlign: "center"}}>4,340,000</div>
-     </div>
-     <div style={{display: "inline-flex" ,flexDirection: "column", gap : "10px"}}>
-      <img src="https://picsum.photos/id/3/400/400"/>
-      <div style={{textAlign: "center",fontWeight:"bold", color: "#454545"}}>MAC BOOK PRO MAX</div>
-      <div style={{textAlign: "center"}}>4,340,000</div>
-     </div>
-     </div>
+      <div style={{display:'flex', gap:'10px'}}>
+        <ProductListItem />
+
+        <ProductListItem />
+
+        <ProductListItem />
+      </div>
 
     </>
   );
---
+++ src/ProductListItem.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function ProductListItem() {
+  const imgNo = 1;
+  const productName = 'MAC BOOK AIR';
+  const productPriceFormatted = '4,340,000';
+
+  
+  return (
+    <>
+      <div
+        style={{
+          display: "inline-flex",
+            flexDirection: "column",
+              gap: "10px",
+        }}
+        >
+        <img src={`https://picsum.photos/id/${imgNo}/400/400`} />
+        <div style={{textAlign:'center', fontWeight:'bold', color:'#454545'}}>{productName}</div>
+        <div style={{textAlign:'center', color:'#454545'}}>{productPriceFormatted}원</div>
+      </div>
+    </>
+  );
+}
+
+
+export default ProductListItem;
+
