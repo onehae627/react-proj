828e0d9d88866bbb790116565ed31593e798a178
onehae627 <onehae627@gmail.com>
2022-12-16 21:17:23 +0900

    배열 렌더링 - 숫자가 교체되는게 아니라 쌓이도록

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
 function NoRecord() {
-  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
-
-  setTimeout(() => {
-    setArr([...arr, arr.length + 1]);
-  }, 1000);
-  const [no, setNo] = useState(10);
-  const [recordedNo, setRecordedNo] = useState(0);
+  const [no, setNo] = useState(0);
+  const [recordedNos, setRecordedNos] = useState([]);
   const saveNo = () => {
-    setRecordedNo(no);
+    setRecordedNos([...recordedNos, no]);
   };
 
   return (
     <>
-      {/* <h1>숫자기록</h1>
+      <h1>숫자기록</h1>
       <input
         type="number"
         value={no}
@@ -24,8 +19,7 @@ function NoRecord() {
         기록
       </button>
       <hr />
-      기록된 숫자 : {recordedNo} */}
-      {arr.join(",")};
+      기록된 숫자 : [{recordedNos.join(",")}]
     </>
   );
 }
