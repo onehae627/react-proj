8a246c05f010ff8c4eb6d6c194bb891bd35da39c
onehae627 <onehae627@gmail.com>
2022-12-20 19:27:49 +0900

    숫자 기록 마무리

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -3,13 +3,6 @@ function NoRecord() {
   const [no, setNo] = useState("");
   const [recordedNos, setRecordedNos] = useState([10, 20, 30]);
   const saveNo = (e) => {
-    e.preventDefault();
-    // prevent 막는다.
-    // Default 기본적인거
-    // 링크를 클릭 했을 때 이동을 막는다.
-    // 폼 전송시 폼이 발송되는 것을 막는다.
-    // 마우스 휠 => 스크롤바 이동을 막는다.
-
     if (no === "") {
       alert("숫자를 입력해주세요.");
       return;
@@ -23,14 +16,19 @@ function NoRecord() {
   return (
     <>
       <h1>숫자기록</h1>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo();
+        }}
+      >
         <input
           type="number"
           value={no}
           onChange={(e) => setNo(e.target.valueAsNumber)}
           className="input w-full max-w-xs"
         />
-        <button type="submit" onClick={saveNo} className="btn btn-outline">
+        <button type="submit" className="btn btn-outline">
           기록
         </button>
       </form>
