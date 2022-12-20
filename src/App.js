0d09dcc275cb73cc03d6835f2d3e7a11f0d455d2
onehae627 <onehae627@gmail.com>
2022-12-20 20:50:55 +0900

    form을 사용해서 dom 조작하기 01

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import NumberCounter from "./NumberCounter";
 import Popup from "./Popup";
 import ProdList from "./ProdList";
 import NoRecord from "./NoRecord";
-
+import FormEx from "./FormEx";
 import "./App.css";
 function App() {
   return (
     <>
-      {/* <StopWatch /> */}
-      {/* <NumberCounter /> */}
-      {/* <Popup /> */}
-      {/* <ProdList className="container mx-auto" /> */}
-      <NoRecord />
+      <FormEx />
     </>
   );
 }
