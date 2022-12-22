62c9f182c664a94deeb6a62b65022f87d5dcd817
onehae627 <onehae627@gmail.com>
2022-12-22 20:43:20 +0900

    숫자 리스트에서 특정 숫자 제거, 첫 숫자 제거, 마지막 숫자 제거 기능 추가

--- src/RefEx.js
+++ src/RefEx.js
@@ -4,21 +4,31 @@ function RefEx() {
   const noInputRef = useRef(null);
   const [no, setNo] = useState("");
 
-  const [recordedNos, setRecordedNos] = useState([10, 20, 30]);
+  const [recordedNos, setRecordedNos] = useState([5, 10, 15, 5, 20, 25, 5, 30]);
 
   const saveNo = () => {
     if (no === "") {
       alert("숫자를 입력해주세요.");
       return;
     }
-
     setRecordedNos([...recordedNos, no]);
     setNo("");
     noInputRef.current.focus();
   };
-
-  //const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
-  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
+  const removeNo5 = () => {
+    const newRecordedNo5 = recordedNos.filter((el) => el != 5);
+    setRecordedNos(newRecordedNo5);
+  };
+  const removeNoFirst = () => {
+    const newRecordedNo5 = recordedNos.filter((_, index) => index != 0);
+    setRecordedNos(newRecordedNo5);
+  };
+  const removeNoLast = () => {
+    const newRecordedNo5 = recordedNos.filter(
+      (_, index) => index != recordedNos.length - 1
+    );
+    setRecordedNos(newRecordedNo5);
+  };
 
   return (
     <>
@@ -29,32 +39,25 @@ function RefEx() {
         }}
       >
         <input
-          ref={noInputRef}
           type="number"
+          ref={noInputRef}
           value={no}
           onChange={(e) => setNo(e.target.valueAsNumber)}
         />
         <button type="submit">기록</button>
       </form>
 
-      <hr />
-
-      <h1>기록된 숫자 v1</h1>
-      {recordedNos.join(",")}
-
-      <hr />
-
-      <h1>기록된 숫자 v2</h1>
-      <ul>{li}</ul>
-
-      <hr />
-
-      <h1>기록된 숫자 v2-2</h1>
+      <h1>기록된 숫자</h1>
       <ul>
         {recordedNos.map((el, index) => (
           <li key={index}>{el}</li>
         ))}
       </ul>
+
+      <hr />
+      <button onClick={removeNo5}> 숫자 5 삭제</button>
+      <button onClick={removeNoFirst}>첫번째 숫자 삭제</button>
+      <button onClick={removeNoLast}>마지막 숫자 삭제</button>
     </>
   );
 }
