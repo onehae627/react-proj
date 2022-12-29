371a344f630e17fc0e3d361f4ebdc2d32db5dec6
onehae627 <onehae627@gmail.com>
2022-12-29 20:25:56 +0900

    useEffect의 의존성 배열을 이용해서 실행민감도 조절

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
 let AppCallCount = 0;
 let SubCallCount = 0;
 
-function Sub() {
+function Sub({ appNo }) {
   SubCallCount++;
   console.log(`Sub이 ${SubCallCount}번 실행됨!`);
   const [no, setNo] = useState(0);
+  const [no2, setNo2] = useState(0);
+
+  useEffect(() => {
+    console.log("effect 1 : 단 한 번 실행");
+  }, []);
+
+  useEffect(() => {
+    console.log("effect 2 : 부모(App)의  appNo가 바뀔 때 마다 실행");
+  }, [appNo]);
+
+  useEffect(() => {
+    console.log("effect 3 : 나(Sub)의  no가 바뀔 때 마다 실행");
+  }, [no]);
+
+  useEffect(() => {
+    console.log("effect 4 : appNo 혹은 no가 변경될 때 마다 실행");
+  }, [appNo, no]);
+
+  useEffect(() => {
+    console.log("effect 5 : 매 번 실행");
+  });
 
   return (
     <>
       <div style={{ border: "10px solid blue", padding: "10" }}>
-        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
+        App no : {appNo}
+        <hr />
+        <button onClick={() => setNo(no + 1)}>버튼1 : {no}</button>
+        <button onClick={() => setNo2(no2 + 1)}>버튼2 : {no2}</button>
       </div>
     </>
   );
@@ -29,7 +53,7 @@ function App() {
       <div style={{ border: "10px solid red", padding: "10" }}>
         <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
         <hr />
-        <Sub />
+        <Sub appNo={no} />
       </div>
     </>
   );
