695985058e87d589312ee87beef93390e8ca955c
onehae627 <onehae627@gmail.com>
2023-01-03 19:46:16 +0900

    useCallback을 사용하여 리랜더링 막기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 
 import "./App.css";
 
 let SubCallCount = 0;
-function Sub({ no1, no2 }) {
+function Sub({ no1, no2, calculateFunc }) {
   SubCallCount++;
   console.log(`SubCallCount: ${SubCallCount}`);
 
   return (
     <>
       <div style={{ border: "5px solid red", padding: 10 }}>
-        서브:{no1 + no2}
+        입력:{no1}, {no2}
+        <br />
+        결과: {calculateFunc(no1, no2)}
       </div>
     </>
   );
 }
 
 let AppCallCount = 0;
-
+// 렌더링 막음.
 const MemorisedSub = React.memo(Sub);
+
 function App() {
   AppCallCount++;
   console.log(`AppCallCount: ${AppCallCount}`);
 
-  const [no, setNo] = useState(0);
+  const [no1, setNo1] = useState(0);
+  const [no2, setNo2] = useState(0);
+
+  const calculateFunc = useCallback((a, b) => a + b, []);
+
   return (
     <>
       <div>안녕하세요</div>
-      <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
+      <button onClick={() => setNo1(no1 + 1)}>버튼1 : {no1}</button>
       <hr />
-      <MemorisedSub no1={10} no2={no} />
+      <button onClick={() => setNo2(no2 + 1)}>버튼2 : {no2}</button>
+      <hr />
+      <MemorisedSub no1={10} no2={20} calculateFunc={calculateFunc} />
     </>
   );
 }
