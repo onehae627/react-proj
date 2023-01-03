2aab14b75485026be38128136adcb7d86060d464
onehae627 <onehae627@gmail.com>
2023-01-03 21:26:25 +0900

    옵션의 선택개수 표시

--- src/Order.js
+++ src/Order.js
@@ -25,6 +25,7 @@ function Order() {
 
   // optionCheckeds가 모두가 참일 때 전체선택 됨
   const btnAllChecked = optionCheckeds.every((el) => el);
+  const selectedCount = optionCheckeds.filter((el) => el).length;
 
   const toggleAllChecked = () => {
     if (btnAllChecked) {
@@ -40,7 +41,9 @@ function Order() {
   return (
     <>
       <h1>음식주문</h1>
-      <h2>옵션 </h2>
+      <h2>
+        옵션 ({selectedCount} / {options.length})
+      </h2>
       <span
         onClick={toggleAllChecked}
         style={{ paddingLeft: 30, userSelect: "none" }}
