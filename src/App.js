deef612ed2f5ffdabb59c2f95d8a5e7a561e983b
onehae627 <onehae627@gmail.com>
2023-01-12 21:11:45 +0900

    셀릭트 박스 다루기

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [selectsGender, setSelectsGender] = useState('W');
+    const ageBands =["영유아/아동", "10대", "20대", "30대", "40대", "50대", "60대", " 그 외"];
+
+    const [selectsAgeBand, setSelectsAgeBand] = useState(ageBands[0]);
   return (
     <>
-      <label>
-        <input type="radio" name="gender" onChange={(e) => setSelectsGender('M')} checked={selectsGender == "M"}/>남성
-        <input type="radio" name="gender" onChange={(e) => setSelectsGender('W')} checked={selectsGender == "W"}/>여성
-      </label>
+      <select onChange={(e) => setSelectsAgeBand(e.target.value)}>
+        <option disabled selected>-나이대역-</option>
+        {ageBands.map((ageBand) => <option selected={ageBand == selectsAgeBand} value={ageBand}>{ageBand}</option>)}
+      </select>
       <hr />
-      <div>현재 값: {selectsGender}</div>
-      </>
+      <div>현재 값: {selectsAgeBand}</div>
+    </>
   );
 }
 
