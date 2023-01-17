dfb8288b0f87ecdd3f46411392e8cb0dabcaf5e7
onehae627 <onehae627@gmail.com>
2023-01-17 20:59:28 +0900

    새 할일 폼 만들기

--- src/App.js
+++ src/App.js
@@ -2,61 +2,103 @@ import React, {  useState, useRef } from "react";
 
 import "./App.css";
 
+function NewTodoForm ({todosState}) {
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-function App() {
-  const [todos, setTodos] = useState([]);
-  const lastTodoId = useRef(0);
+    const form = e.target;
 
-  const addTodo = (newcontent) => {
-    const id = lastTodoId.current++;
-    const newTodo = {
-      id,
-      // 문장은 받아온걸로 넣겠다.
-      content : newcontent,
-      regDate: "2023-01-17 12 : 12 : 12"
+    form.content.value = form.content.value.trim();
+
+    if ( form.content.value.length == 0) {
+      alert("할 일을 입력해주세요!!");
+
+      form.content.focus();
+
+      return;
     }
-    // 지난번에 있던애들이랑 새로 추가된 애들까지 같이 추가됨.
-    const newTodos = [...todos, newTodo ];
-    setTodos(newTodos);
+
+    todosState.addTodo(form.content.value);
+    form.content.value = "";
+    form.content.focus();
+
   }
 
-  const removeTodo = (index) => {
-    // 해당 인덱스 빼고 다 필터링한다.~~
-    const newTodos = todos.filter((_, _index) => _index != index);
-    setTodos(newTodos);
-  }
-
-  const modifyTodo = (index, newcontent) => {
-    const newTodos = todos.map((todo, _index) => _index != index ? todo : {...todo, content: newcontent})
-    setTodos(newTodos);
-  }
-  // 추가 버튼을 누르면addTodo실행됨. 
-  const onBtnAddTofoClick = () => {
-    addTodo("안녕");
-  }
-
-  const onBtnDeleteTofoClick = () => {
-    removeTodo(1);
-  }
-
-  const onBtnModifyTofoClick = () => {
-    modifyTodo(1, "헤헷");
-  }
   return (
     <>
-      <button onClick={onBtnAddTofoClick}>추가</button>
-      <button onClick={onBtnDeleteTofoClick}>삭제</button>
-      <button onClick={onBtnModifyTofoClick}>수정</button>
+    <form onSubmit={onSubmit}>
+      <input autoComplete="off" name="content" type="text" placeholder="할 일을 입력해주세요."/>
+      <input type="submit" value="추가"/>
+      <input type="reset" value="취소"/>
+    </form>
+    </>
+  )
+
+}
+
+
+
+function TodoApp({ todosState }) {
+  
+
+  return (
+    <>
+      <NewTodoForm todosState={todosState}/>
       <hr />
       <ul>
-        {todos.map((todo, index) => (
+        {todosState.todos.map((todo, index) => (
           <li key={index}>
-            {todo.id}
-            {todo.content}
-            {todo.regDate}
+            {todo.id} {todo.content} {todo.regDate} 
           </li>
         ))}
       </ul>
+    </>
+  );
+}
+
+function useTodosState() {
+  const [todos, setTodos] = useState([]);
+  const lastTodoIdRef = useRef(0);
+
+  const addTodo = (newContent) => {
+    const id = ++lastTodoIdRef.current;
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: "2023-01-17 12:12:12"
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : { ...todo, content: newContent }
+    );
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    modifyTodo,
+    removeTodo
+  };
+}
+
+function App() {
+  const todosState = useTodosState();
+
+  return (
+    <>
+      <TodoApp todosState={todosState} />
     </>
   );
 }
